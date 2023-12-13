6f1cc26e19c9008a78dce883003b82ef50a7008b
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2023-12-13 10:17:11 +0000

    start

--- TechnicalSolution/TechnicalSolution/Program.cs
+++ TechnicalSolution/TechnicalSolution/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var lines = File.ReadAllLines("names.txt"); //read the text file into the program
+
+            for (int i = 0; i < lines.Length; i++) //convert the lines into an array to manipulate and read. 
+            {
+                var unsorted = lines[i].Split(' ');
+            }
+
+            //test one, to make sure that the array is read correctly. 
+            Console.WriteLine(lines[0]);
+
+            //test two, to see if the array has been sorted. 
+
         }
     }
 }
