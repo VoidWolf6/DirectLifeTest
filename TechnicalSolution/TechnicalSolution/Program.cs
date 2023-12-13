eab630a88db2638a98a90b21a20d506e1d7dca38
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2023-12-13 16:44:26 +0000

    final?

--- TechnicalSolution/TechnicalSolution/Program.cs
+++ TechnicalSolution/TechnicalSolution/Program.cs
@@ -5,10 +5,12 @@ namespace TechnicalSolution
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
+            //65 is the offset for capital A
             List<string> unsorted = new List<string>();
-            string lines = File.ReadAllText(@"names.txt").ToString(); //read the text file into the program as a string
+            string lines = File.ReadAllText("names.txt").ToString(); //read the text file into the program as a string
             var results = lines.Split(","); //splits into an array, divided by the commas
 
             //convert the lines into an array to manipulate and read.
@@ -20,17 +22,40 @@ namespace TechnicalSolution
             //test one, to make sure that the array is read correctly. 
             Console.WriteLine(unsorted[0]);
 
-            List<string> sorted = unsorted.OrderBy(x => x).ToList(); //sorts the list into alphabetical order.
+            List<string> sorted = unsorted.OrderBy(x => x).ToList(); //sorts the list into alphabetical order
 
 
             //test two, to see if the array has been sorted. 
             Console.WriteLine(sorted[0]);
 
-
-            for (int i = 0; i < sorted[0].Length; i++)
+            int testTotal = 0, subTotal = 0, finalTotal = 0;
+            foreach (char each in sorted[0]) //gathers the alphabetical value of the first item in the sorted array
             {
-
+                testTotal += each - 64; //offset is decreased by 1, to make sure the values are correct e.g. that 'A' returns 1
             }
+
+            //test three, to make sure the alphabetical total functions work
+            Console.WriteLine(testTotal);
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                foreach (char letter in sorted[i])
+                {
+                    subTotal += letter - 64;
+                }
+
+                finalTotal += subTotal * (i + 1); //multiplies the subtotal of each word by its position, with 1 added as i is initialised to 0
+                subTotal = 0;
+
+                //test four, to see if the function is working by testing the first six words
+                if (i == 5)
+                {
+                    Console.WriteLine(finalTotal);
+                }
+            }
+
+            //delivers the final total of the array
+            Console.WriteLine(finalTotal);
         }
     }
 }
